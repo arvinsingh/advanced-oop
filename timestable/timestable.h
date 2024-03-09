d795e9ba7bcc4ab08d610432618e4f35a4915beb
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-03-09 22:36:09 +0000

    added(timestable header): for definitions

---
+++ timestable/timestable.h
@@ -0,0 +1,14 @@
+// Owner: Arvin Singh
+
+#ifndef TIMESTABLE_H
+#define TIMESTABLE_H
+
+#define MAX_TABLE_SIZE 13
+
+#define MAX_TIMES_TABLE 12
+
+void generateTable(int num, int *table);
+void printTable(int num, int *table);
+void printTables(int **tables);
+
+#endif // TIMESTABLE_H
