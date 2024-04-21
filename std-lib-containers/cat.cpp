bf246f3efe72e2932b705ca21a74cc2805e6b988
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-04-21 01:30:22 +0100

    modified(cat.cpp): implemented constructors, hash fn, operators

--- std-lib-containers/cat.cpp
+++ std-lib-containers/cat.cpp
@@ -1,5 +1,9 @@
+// Owner: Arvin Singh
+
 #include "cat.h"
 
+Cat::Cat(): Cat("Oggy", 10) {}
+Cat::Cat(std::string name): Cat(name, 10) {}
 Cat::Cat(const std::string& name, unsigned int lives) : name(name), lives(lives) {}
 
 // Getter implementations
@@ -24,4 +28,18 @@ void Cat::setLives(unsigned int lives) {
 std::ostream& operator<<(std::ostream& os, const Cat& cat) {
     os << "Name: " << cat.name << ", Lives: " << cat.lives;
     return os;
+}
+
+// Friend equality operator implementation
+bool operator==(const Cat &left, const Cat &right) {
+  return left.getName()  == right.getName()   &&
+         left.getLives() == right.getLives();
+}
+
+// Friend comparasion operator implementation
+bool operator<(const Cat &left, const Cat &right) {
+  if (left.getName() == right.getName()) {
+    return left.getLives() < right.getLives();
+  }
+  return left.getName() < right.getName();
 }
